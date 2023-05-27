9ab7982bf7710c4b5760bb3d180a0380ddc92ce8
CadinCtn <cristyanrochaoff@gmail.com>
2023-05-27 16:19:03 -0300

    dei continuidade na fase 2

--- src/Jogo_aula2/Jogo.java
+++ src/Jogo_aula2/Jogo.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 
 public class Jogo {
 
+    //variaveis globais
     static Scanner le = new Scanner(System.in);
-    static byte fuga = 0;
-    static byte dado;
+    static byte fuga = 0, dado,re;
+
+    //funções:
     public static void morte() {
         System.out.println("Você morre, um trágico fim para um guerreiro tão jovem, é uma pena.");
     }
@@ -17,13 +19,69 @@ public class Jogo {
         return d20.nextInt(19)+1;
     }
 
+    public static int fuga_fase2() {
+        byte a,d;
+
+        System.out.println("A criatura estranha te ataca!");
+        System.out.println("O que quer fazer:");
+        System.out.println("1) Revidar  -  2) Tentar escapar  -  3) Não fazer nada");
+        d = le.nextByte();
+
+        switch (d) {
+            case 1:
+                System.out.println("A criatura vê você se posicionando e tomando uma postura mais confiante, então se acovarda e foge para a caverna.");
+                break;
+
+
+            case 2:
+                dado = (byte) d20();
+                System.out.println("Role um D20:");
+                a = le.nextByte();
+                System.out.println(dado);
+                if (dado == 1) {
+                    System.out.println("Você tropeça, cai, e é devorado!");
+                    morte();
+                    re = 1;
+                } else if (dado > 1 && dado < 11) {
+                    System.out.println("Você tenta corre mas da de cara em uma árvore, a criatura te alcança e te devora!");
+                    morte();
+                    re = 1;
+                } else if(dado ==20){
+                    System.out.println("Por sorte (muita sorte), o Alotimantes perde você e voê consegue escapar");
+
+                } else {
+                    System.out.println("Você entra em uma mata fechada com uma densa neblina, corre o mais rápido que consegue, sua vida depende disso, '...não interessa onde isso vai dar só preciso fugir!', você pensa.");
+                    System.out.println("Antes que perceba acaba tropeçando por causa de sua visão limitada devido a densa neblina, se levanta rapidamente e descobre que não tem mais chão, você caiu de um penhasco.");
+                    System.out.println("De repente tudo se apaga e...");
+                    morte();
+                    re = 1;
+                }
+                break;
+
+
+            case 3: System.out.println("Você fica parado em uma postura ereta, sem medo, sem desconfiança, sem emoções, apenas aceitando seu destino final chegar...");
+                System.out.println("A criatura foje, parece não gostar de coisas sem desafio.");
+                break;
+
+
+            default: System.out.println("Você fica parado indeciso com uma cara de alguem que gosta de fazer o que não deve!");
+                morte();
+                System.out.println("Bem feito!");
+                re = 1;
+
+                break;
+
+        }
+    return 0;
+    }
+
     public static void fase1() {
         byte x = 0, z = 0;
         String a;
 
 
         System.out.println("Você estava caminhando pela floresta e encontra uma bifurcação, um caminho para direita e um para esquerda, por onde decide ir?");
-        System.out.println("1) Esquerda'CAVERNA'  -  2) Direita'CAMINHO DA DOR'");
+        System.out.println("1) Esquerda'CAVERNA'  -  2) Direita'continuar a trilha'");
         x = le.nextByte();
         dado = (byte) d20();
 
@@ -31,6 +89,7 @@ public class Jogo {
 
             case 1:
                 System.out.println("Você adentrou a caverna!");
+                System.out.println("Teste de Sorte:");
                 System.out.println("Role o D20:");
                 a = le.next();
                 System.out.println(dado);
@@ -38,7 +97,7 @@ public class Jogo {
                 if (dado == 1) {
                     System.out.println("O chão desaba, você tem uma queda terrivel!");
                     morte();
-                } else if (dado > 1 && dado < 11) {
+                } else if (dado > 1 && dado < 16) {
                     System.out.println("Voce ouve um barulho vindo de sua direita, olha naquela direção e vê um 'Alotimantes'!");
                     System.out.println("Uma criatura comum por essas cavernas, o que quer fazer?");
                     System.out.println("1) Fugir  -  2) Enfrentá-lo");
@@ -58,7 +117,7 @@ public class Jogo {
                             System.out.println("Você fica indeciso e o Alomantes te ataca sem chances de revidar ou se defender, devido a sua indecisão!");
                             morte();
                     }
-                } else if (dado < 20 && dado >= 11) {
+                } else if (dado < 20 && dado >= 16) {
                     System.out.println("Você atravessa a caverna sem complicações!");
                 }
 
@@ -92,64 +151,60 @@ public class Jogo {
 
     public static int fase2() {
 
-        byte d,a;
+        byte d,a,emboscada;
 
         if(fuga == 1) {
-            System.out.println("A criatura estranha te ataca!");
-            System.out.println("O que quer fazer:");
-            System.out.println("1) Revidar  -  2) Tentar escapar  -  3) Não fazer nada");
-            d = le.nextByte();
+            fuga_fase2();
+        }
 
-            switch (d) {
-                case 1:
-                    System.out.println("A criatura vê você se posicionando e tomando uma postura mais confiante, então se acovarda e foge para a caverna.");
-                    break;
+        if(re == 0) {
+            System.out.println("Depois disso, ao sair da caverna, encontra com facilidade a trilha novamente e continua a caminhar, em busca de um novo desafio.");
+            System.out.println("Teste de percepção:");
+            System.out.println("Role um D20:");
+            dado =d20();
+            a = le.nextByte();
+            System.out.println(dado);
+            if(dado == 1) {
+                System.out.println("Você vê uma figura humanoide mais adiante a sua frente, ouve passos atraz das árvores, arbustos se mexendo.");
+                System.out.println("Você foi emboscado por ladrões, malfeitores que vagam por aí para saquear de qualquer aventureiro novato que esteja distraido demais para percebe-los");
+                System.out.println("'Renda-se, deixe tudo que tem e te deixaremos ir'");
+                System.out.println("1) Se render  -  2) Lutar em desvantagem");
+                    switch (emboscada){
+                        case 1: System.out.println("Você deixa suas coisas, seu dinheiro, seus pertences.");
+                                System.out.println("Após pegarem suas coisas ao verem que você não tem mais utilidade, os arqueiros disparam suas flechas em você.");
+                                System.out.println("Gritando de dor se arrepende de sua decisão covarde, em poucos momentos sua visão escurece.");
+                                morte();
+                                re = 1;
+                            break;
 
+                        case 2: System.out.println("Você saca sua espada e se prepara para a luta");
+                                System.out.println("No momento em que da um passo a frente, antes de perceber você é alvejado por varias flechas.");
+                                morte();
+                                re = 1;
+                                break;
 
-                case 2:
-                    dado = (byte) d20();
-                    System.out.println("Role um D20:");
-                    a = le.nextByte();
-                    if (dado == 1) {
-                        System.out.println("Você tropeça, cai, e é devorado!");
-                        morte();
-                    } else if (dado > 1 && dado < 11) {
-                        System.out.println("Você tenta corre mas da de cara em uma árvore, a criatura te alcança e te devora!");
-                        morte();
-                    } else if(dado ==20){
-                    System.out.println("Por sorte (muita sorte), o Alotimantes perde você e voê consegue escapar");
-
-                    } else {
-                            System.out.println("Você entra em uma mata fechada com uma densa neblina, corre o mais rápido que consegue, sua vida depende disso, '...não interessa onde isso vai dar só preciso fugir!', você pensa.");
-                            System.out.println("Antes que perceba acaba tropeçando por causa de sua visão limitada devido a densa neblina, se levanta rapidamente e descobre que não tem mais chão, você caiu de um penhasco.");
-                            System.out.println("De repente tudo se apaga e...");
-                            morte();
+                        default: System.out.println("Uma flecha é disparada e te atinge");
                     }
-                    return 0;
-
-
-                case 3: System.out.println("Você fica parado em uma postura ereta, sem medo, sem desconfiança, sem emoções, apenas aceitando seu destino final chegar...");
-                        System.out.println("A criatura foje, parece não gostar de coisas sem desafio.");
-                        break;
-
-
-                default: System.out.println("Você fica parado indeciso com uma cara de alguem que gosta de fazer o que não deve!");
-                        morte();
-                        System.out.println("Bem feito!");
-
-                    return 0;
-
             }
         }
 
-        System.out.println("Ao sair da caverna...");
 
-    return 0;
+        return 0;
     }
 
     public static void main(String[] args) {
+
+        //prólogo
+        System.out.println("Este é um mundo medieval, onde existem centenas de aventureiros buscando se tornarem grandes lendas.");
+        System.out.println("Voce, é um desses aventureiros.");
+        System.out.println("Você está vagando por aí em busca de grandes desafios, com este objetivo, foi a floresta sombria, lugar onde existem diversas criaturas.");
+
+
+        //começo
         fase1();
-        fase2();
+            if(re == 0) {
+                fase2();
+            }
     }
 
 }
